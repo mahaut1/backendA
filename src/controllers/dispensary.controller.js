45241d19cba0f2f8cb4c7886cfa4aa02f429fb98
CharlesDesc06 <charles.deschamps06@hotmail.com>
2025-12-09 15:39:58 +0100

    implement dispensary controller with initial methods

---
+++ src/controllers/dispensary.controller.js
@@ -0,0 +1,21 @@
+// dispensary.controller.js
+
+export const createDispensary = (req, res) => {
+	throw new Error("Not implemented"); // TODO: Implement createDispensary
+};
+
+export const getAllDispensaries = (req, res) => {
+	throw new Error("Not implemented"); // TODO: Implement getAllDispensaries
+};
+
+export const getDispensaryById = (req, res) => {
+	throw new Error("Not implemented"); // TODO: Implement getDispensaryById
+};
+
+export const patchDispensaryById = (req, res) => {
+	throw new Error("Not implemented"); // TODO: Implement patchDispensaryById
+};
+
+export const deleteDispensaryById = (req, res) => {
+	throw new Error("Not implemented"); // TODO: Implement deleteDispensaryById
+};
